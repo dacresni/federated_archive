b169d246a9483f97d8ca4b7bba1e409151506cb6
Blane Herndon <144256091+driverextend@users.noreply.github.com>
2025-02-22 16:41:25 -0600

    Add very important comments

--- redirect.js
+++ redirect.js
@@ -1,18 +1,20 @@
-async function search() { 
-  let term = document.getElementById("website").value
-  let stamp = document.getElementById("date").valueAsNumber
-  stamp = Date(stamp)
-  const url = `http://archive.org/wayback/available?url=${term}&timestamp=${stamp}`
-  try{ 
-    const response  = await fetch(url)
+// LOOK AT THE SLACK
+
+async function search() {
+  let term = document.getElementById("website").value;
+  let stamp = document.getElementById("date").valueAsNumber;
+  stamp = Date(stamp);
+  const url = `http://archive.org/wayback/available?url=${term}&timestamp=${stamp}`;
+  try {
+    const response = await fetch(url);
     if (!response.ok) {
       throw new Error(`Response status: ${response.status}`);
     }
     const json = await response.json();
     console.log(json);
-    available = json.archived_snapshots.closest.available 
-    if(available) {
-      location.assign(json.archived_snapshots.closest.url)
+    available = json.archived_snapshots.closest.available;
+    if (available) {
+      location.assign(json.archived_snapshots.closest.url);
     }
   } catch (error) {
     console.error(error.message);
