1a1d2fa003498c3bfa7931efe9b3c8296a59b812
Nehemiah Dacres <vivacarlie@gmail.com>
2025-02-22 14:55:58 -0600

    handle json respons

--- redirect.js
+++ redirect.js
@@ -1,14 +1,17 @@
-function search(term) { 
+function search(term, stamp) { 
   const url = `http://archive.org/wayback/available?url=${term}`
   try{ 
     const response  = await fetch(url)
     if (!response.ok) {
-      throw new Error(`Response status: ${response.status}`);
+      throw new Error(`Response status: ${response.status}&timestamp=${stamp}`);
     }
     const json = await response.json();
     console.log(json);
+    available = json.archived_snapshots.closest.available 
+    if(available) {
+      location.assign(json.archived_snapshots.closest.url)
+    }
   } catch (error) {
     console.error(error.message);
   }
-
 }
