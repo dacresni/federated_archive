c4f3105eeaab78730a22175fd3358cc8170682dc
Nehemiah Dacres <vivacarlie@gmail.com>
2025-02-23 09:11:28 -0600

    parse date into timestamp for api

--- redirect.js
+++ redirect.js
@@ -1,20 +1,27 @@
 async function search() { 
   let term = document.getElementById("website").value
-  let stamp =document.getElementById("date").valueAsNumber
-  let stamp = Date(stamp)
-  const url = `http://archive.org/wayback/available?url=${term}&timestamp=${stamp}`
-  try{ 
-    const response  = await fetch(url)
-    if (!response.ok) {
-      throw new Error(`Response status: ${response.status}`);
+  let date =document.getElementById("date").valueAsNumber
+  if (stamp) {
+    let stamp = Date(date)
+    let timestamp = `${stamp.getFullYear()}${stamp.getMonth()}${stamp.getDay()}${stamp.getHours()}${stamp.getMinutes()}${stamp.getSeconds()}`
+    // YYYYMMDDhhmmss 
+    const url = `http://archive.org/wayback/available?url=${term}&timestamp=${stamp}`
+  } else {
+    const url = `http://archive.org/wayback/available?url=${term}`
+
+    try{ 
+      const response  = await fetch(url)
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+      const json = await response.json();
+      console.log(json);
+      available = json.archived_snapshots.closest.available 
+      if(available) {
+        location.assign(json.archived_snapshots.closest.url)
+      }
+    } catch (error) {
+      console.error(error.message);
     }
-    const json = await response.json();
-    console.log(json);
-    available = json.archived_snapshots.closest.available 
-    if(available) {
-      location.assign(json.archived_snapshots.closest.url)
-    }
-  } catch (error) {
-    console.error(error.message);
   }
 }
