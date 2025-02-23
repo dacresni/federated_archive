449e8e5b804fb383ff713b8cd6b631223c256343
Nehemiah Dacres <vivacarlie@gmail.com>
2025-02-23 10:37:00 -0600

    helpfull logging

--- redirect.js
+++ redirect.js
@@ -1,21 +1,22 @@
 async function search() { 
   let term = document.getElementById("website").value
   let date =document.getElementById("date").valueAsNumber
-  if (stamp) {
-    let stamp = Date(date)
-    let timestamp = `${stamp.getFullYear()}${stamp.getMonth()}${stamp.getDay()}${stamp.getHours()}${stamp.getMinutes()}${stamp.getSeconds()}`
+  var url = ''
+  if (date != '') {
+    var stamp = new Date(date)
+    let timestamp =`${stamp.getFullYear()}${stamp.getMonth()}${stamp.getDay()}${stamp.getHours()}${stamp.getMinutes()}${stamp.getSeconds()}`
+    console.log("timestamp: ${timestamp}" )
     // YYYYMMDDhhmmss 
-    const url = `http://archive.org/wayback/available?url=${term}&timestamp=${stamp}`
+    url = `http://archive.org/wayback/available?url=${term}&timestamp=${stamp}`
   } else {
-    const url = `http://archive.org/wayback/available?url=${term}`
-
+    url = `http://archive.org/wayback/available?url=${term}`
     try{ 
       const response  = await fetch(url)
       if (!response.ok) {
         throw new Error(`Response status: ${response.status}`);
       }
       const json = await response.json();
-      console.log(json);
+      console.log('json:'+json);
       available = json.archived_snapshots.closest.available 
       if(available) {
         location.assign(json.archived_snapshots.closest.url)
@@ -23,4 +24,5 @@ async function search() {
     } catch (error) {
       console.error(error.message);
     }
-
+  }
+}
